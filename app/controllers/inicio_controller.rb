995ba1a59a252c1e26b5eb33dd9c701e3348a7f3
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-10-04 13:19:34 -0500

    Intentando obtener los datos constantemente

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -4,7 +4,6 @@ class InicioController < ApplicationController
     @hashtags = Hashtag.maximum(:id)
     @usuarios = Usuario.maximum(:id)
     respond_to do |format|
-      format.html
       format.js {}
       format.json { render json: {:tweets => @tweets, :hashtags => @hashtags, :usuarios => @usuarios}}
     end
