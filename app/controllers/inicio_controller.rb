bf9c2241baa1f4a5ef35a216d2ed4a2d2d3dbc83
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-10-04 13:24:37 -0500

    Intentando obtener los datos constantemente

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,21 +1,13 @@
 class InicioController < ApplicationController
   def buscar
-		@tweets = Tweet.maximum(:id)
+    @tweets = Tweet.maximum(:id)
     @hashtags = Hashtag.maximum(:id)
     @usuarios = Usuario.maximum(:id)
-    respond_to do |format|
-      format.js {}
-      format.json { render json: {:tweets => @tweets, :hashtags => @hashtags, :usuarios => @usuarios}}
-    end
   end
 
   def obtener_stats
 		@tweets = Tweet.maximum(:id)
     @hashtags = Hashtag.maximum(:id)
     @usuarios = Usuario.maximum(:id)
-    respond_to do |format|
-      format.js {}
-      format.json { render json: {:tweets => @tweets, :hashtags => @hashtags, :usuarios => @usuarios}}
-    end
   end
 end
