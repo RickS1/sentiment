b0f7b583c5131224b79bf79833b477ba0838d63b
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-10-04 12:51:33 -0500

    Integrando Postgresql

--- app/assets/javascripts/inicio.js
+++ app/assets/javascripts/inicio.js
@@ -1,6 +1,18 @@
 
 
 $(document).ready(function(){
+	$.ajax({
+		url: '/inicio/obtener_stats',
+		type: 'GET',
+		cache: false,
+		success: function(data) {
+			stats = JSON.parse(data);
+			$("#resultados").html("Tweets: " + stats.tweets + "\nHashtags: " + stats.hashtags + "\nUsuarios mencionados: " + stats.usuarios );
+		},
+		error: function() {
+			$("#resultados").html("Error al recoger los datos.");
+		}
+	});
 	setInterval(function(){
 		$.ajax({
 			url: '/inicio/obtener_stats',
