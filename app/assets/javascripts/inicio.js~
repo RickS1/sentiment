8d3fb1911900380baa7c6bd8024c068119c19f98
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-10-31 14:23:58 -0600

    Integrando d3

--- app/assets/javascripts/inicio.js~
+++ app/assets/javascripts/inicio.js~
@@ -1,8 +1,8 @@
-$(function(){
+/*$(function(){
 	setTimeout(refreshData,5000);
 });
 
 function refreshData(){
 	$.getScript("/inicio/obtener_stats.js");
   setTimeout(refreshData,5000);
-}
+}*/
--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,13 +1,13 @@
 class InicioController < ApplicationController
   def buscar
-    @tweets = Tweet.maximum(:id)
-    @hashtags = Hashtag.maximum(:id)
-    @usuarios = Usuario.maximum(:id)
+    #@tweets = Tweet.maximum(:id)
+    #@hashtags = Hashtag.maximum(:id)
+    #@usuarios = Usuario.maximum(:id)
   end
 
   def obtener_stats
-		@tweets = Tweet.maximum(:id)
-    @hashtags = Hashtag.maximum(:id)
-    @usuarios = Usuario.maximum(:id)
+		#@tweets = Tweet.maximum(:id)
+    #@hashtags = Hashtag.maximum(:id)
+    #@usuarios = Usuario.maximum(:id)
   end
 end
