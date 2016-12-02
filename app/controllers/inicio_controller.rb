8f78101e4ef351b98f984cdd7c120137d4bbb2e0
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-12-02 13:24:40 -0600

    Prueba con YouTube

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -4,8 +4,6 @@ class InicioController < ApplicationController
 
   def videos
     @vids = Yt::Collections::Videos.new
-    #vids.where(order: 'viewCount').first.title
-    @videos = @vids.where(q: 'Colegio de México PRED')
 
     @video = Yt::Video.new id: 'wdW3o6nwOfY'
   end
