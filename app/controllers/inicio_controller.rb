217bc29a89a60cc67914ca89d6168397a4b9f818
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-12-02 12:48:05 -0600

    Prueba con YouTube

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -3,11 +3,8 @@ class InicioController < ApplicationController
   end
 
   def videos
-    vids = Yt::Collections::Videos.new
+    @vids = Yt::Collections::Videos.new
     #vids.where(order: 'viewCount').first.title
-    @size = vids.where(q: 'Colegio de México PRED', safe_search: 'none').size #=> 324
-    @videos = vids.where(q: 'Colegio de México PRED', safe_search: 'none')
-    #vids.where(chart: 'mostPopular', video_category_id: 44).first.title #=> "SINISTER - Trailer"
-    #vids.where(id: 'jNQXAC9IVRw,invalid').map(&:title) #=> ["Fullscreen Creator Platform"]
+    @videos = @vids.where(q: 'Colegio de México PRED')
   end
 end
