522be28f7d9efbbc4af9f18fca3fe37477c38927
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-12-05 13:42:22 -0600

    Integración de UIKit

---
+++ public/js/escala_auto.js
@@ -0,0 +1,15 @@
+barra_render = false;
+
+$(document).ready(function(){
+
+	$(document).trigger("scroll");
+});
+
+window.onresize = function(){
+
+	$(document).trigger("scroll");
+}
+
+$(window).on("orientationchange",function(){
+	$(window).trigger("resize");
+});
